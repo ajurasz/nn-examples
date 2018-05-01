3812c3fdf69764cd75818d62a24f154c017b60f0
ajurasz <arkadiusz.jurasz@gmail.com>
2018-05-01 17:22:27 +0200

    Matrix math (clean code).

--- multi/matrix.js
+++ multi/matrix.js
@@ -1,83 +1,29 @@
 class Matrix {
-  constructor(rows, cols, values) {
+  constructor(rows, cols) {
     this.rows = rows;
     this.cols = cols;
-    if (values) {
-      this.values = values;
-    } else {
-      this.values = [];
-      for (let i = 0; i < rows; i++) {
-        this.values[i] = [];
-        for (let j = 0; j < cols; j++) {
-          this.values[i][j] = Math.floor(Math.random() * 10);
-        }
+    this.values = Array(rows)
+      .fill()
+      .map(() => Array(cols).fill(0));
+  }
+
+  map(fn) {
+    for (let i = 0; i < this.rows; i++) {
+      for (let j = 0; j < this.cols; j++) {
+        this.values[i][j] = fn(this.values[i][j], i, j);
       }
     }
   }
 
-  print() {
-    console.table(this.values);
+  randomize() {
+    this.map(_ => Math.floor(Math.random() * 10));
   }
-
   size() {
     console.log('(' + this.rows + ' x ' + this.cols + ')');
   }
 
-  add(n) {
-    const m = new Matrix(this.rows, this.cols, this.values);
-    if (n instanceof Matrix) {
-      for (let i = 0; i < this.rows; i++) {
-        for (let j = 0; j < this.cols; j++) {
-          m.values[i][j] += n.values[i][j];
-        }
-      }
-    } else if (typeof n === 'number') {
-      for (let i = 0; i < this.rows; i++) {
-        for (let j = 0; j < this.cols; j++) {
-          m.values[i][j] += n;
-        }
-      }
-    } else {
-      throw new Error(typeof n + ' is not supported');
-    }
-
-    return m;
-  }
-
-  multiply(n) {
-    if (n instanceof Matrix) {
-      // matrix product
-      if (this.cols !== n.rows) {
-        throw new Error('Columns number is not equalt to rows number');
-      }
-      const m = new Matrix(this.rows, n.cols);
-
-      for (let i = 0; i < m.rows; i++) {
-        for (let j = 0; j < m.cols; j++) {
-          let sum = 0;
-
-          for (let x = 0; x < this.cols; x++) {
-            sum += this.values[i][x] * n.values[x][j];
-          }
-
-          m.values[i][j] = sum;
-        }
-      }
-
-      return m;
-    } else if (typeof n === 'number') {
-      // scalar product
-      const m = new Matrix(this.rows, this.cols, this.values);
-      for (let i = 0; i < this.rows; i++) {
-        for (let j = 0; j < this.cols; j++) {
-          m.values[i][j] *= n;
-        }
-      }
-
-      return m;
-    } else {
-      throw new Error(typeof n + ' is not supported');
-    }
+  print() {
+    console.table(this.values);
   }
 
   transpose() {
@@ -89,5 +35,51 @@ class Matrix {
     }
 
     return m;
+  }
+
+  add(n) {
+    if (n instanceof Matrix) {
+      this.map(value, i, j => value + n.values[i][j]);
+    } else if (typeof n === 'number') {
+      this.map(value => value + n);
+    } else {
+      throw new Error(typeof n + ' is not supported');
+    }
+  }
+
+  multiply(n) {
+    if (typeof n === 'number') {
+      // scalar product
+      this.map(value => value * n);
+    } else {
+      throw new Error(typeof n + ' is not supported');
+    }
+  }
+
+  static multiply(m1, m2) {
+    if (m1 instanceof Matrix && m2 instanceof Matrix) {
+      // matrix product
+      if (m1.cols !== m2.rows) {
+        throw new Error('Columns number is not equalt to rows number');
+      }
+      const m = new Matrix(m1.rows, m2.cols);
+      for (let i = 0; i < m.rows; i++) {
+        for (let j = 0; j < m.cols; j++) {
+          let sum = 0;
+
+          for (let x = 0; x < m1.cols; x++) {
+            sum += m1.values[i][x] * m2.values[x][j];
+          }
+
+          m.values[i][j] = sum;
+        }
+      }
+
+      return m;
+    } else {
+      throw new Error(
+        typeof m1 + ' and ' + typeof m2 + ' needs to be of Matrix type'
+      );
+    }
   }
 }
--- multi/sketch.js
+++ multi/sketch.js
@@ -1,6 +1,17 @@
 function setup() {
   createCanvas(450, 450);
+
+  const m1 = new Matrix(3, 2);
+  m1.randomize();
+  m1.print();
+  const m2 = new Matrix(2, 3);
+  m2.randomize();
+  m2.print();
+
+  const m3 = Matrix.multiply(m1, m2);
+  m3.print();
+  const transpose = m3.transpose();
+  transpose.print();
 }
 
-function draw() {
-}
+function draw() {}
